38cd5407cf7d29d882af620215bf45a6be91cb99
preteekjain98 <prateek9jain8@gmail.com>
2025-04-24 17:59:20 +0530

    Spacing issues

--- src/layouts/components/services/ServicesBanner.tsx
+++ src/layouts/components/services/ServicesBanner.tsx
@@ -30,7 +30,7 @@ interface Props {
 
 export const ServicesBanner: React.FC<Props> = ({ className, content }) => {
   return (
-    <section className="relative z-[1] pt-16 pb-24 lg:pt-24 lg:pb-32">
+    <section className="relative z-[1] pt-16 pb-24 lg:pt-12 lg:pb-32">
       <div className="container mx-auto px-4 md:px-8">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-12 items-center">
           <div className="text-center lg:text-left order-2 lg:order-1">
