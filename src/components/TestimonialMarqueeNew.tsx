1719363cc9b4c0edfc5fb54d19ae0f36eaadb0f7
preteekjain98 <prateek9jain8@gmail.com>
2025-05-02 10:38:11 +0530

    testimonial overlay fix

--- src/components/TestimonialMarqueeNew.tsx
+++ src/components/TestimonialMarqueeNew.tsx
@@ -75,8 +75,8 @@ export const TestimonialMarquee: React.FC<TestimonialMarqueeProps> = ({ data }) 
 
         <div className="mt-16 relative">
           {/* Gradient overlays */}
-          <div className="absolute left-0 top-0 bottom-0 w-40 bg-gradient-to-r from-white to-transparent z-10"></div>
-          <div className="absolute right-0 top-0 bottom-0 w-40 bg-gradient-to-l from-white to-transparent z-10"></div>
+          <div className="absolute left-0 top-0 bottom-0 w-20 bg-gradient-to-r from-white to-transparent z-10"></div>
+          <div className="absolute right-0 top-0 bottom-0 w-20 bg-gradient-to-l from-white to-transparent z-10"></div>
           
           {/* First row */}
           <div className="overflow-hidden">
